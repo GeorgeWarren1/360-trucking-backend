742f7864482f29ad46419199f3500f0f034b5ca7
GeorgeWarren1 <george.warren.pne@gmail.com>
2025-01-12 11:18:08 -0500

    shit

--- app/Http/Middleware/IdentifyTenant.php
+++ app/Http/Middleware/IdentifyTenant.php
@@ -4,7 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use App\Models\Tenant;
-use App\Support\TenantManager;  // <-- Import our new class
+use App\Support\TenantManager;
 
 class IdentifyTenant
 {
